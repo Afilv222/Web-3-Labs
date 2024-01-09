5ff1738cc1a77342feecd8b013a7bb2e93d3427c
Afil Vidyasagar <avidy222@mtroyal.ca>
2024-01-09 15:53:24 -0700

    Started working on test your knowledge 1

--- Lab-Exercise/lab10/js/lab10-ex01.js
+++ Lab-Exercise/lab10/js/lab10-ex01.js
@@ -16,7 +16,13 @@ document.addEventListener('DOMContentLoaded', () => {
                      {id:7, title: "Music in the Tuileries Gardens", artist: "Manet", year: 1862},
                      {id:8, title: "Balcony", artist: "Manet", year: 1868}];
 
-
+   const stocks = [
+      {symbol: "AMZN", name: "Amazon", price: 23.67, units: 59},
+      {symbol: "AMT", name: "American Tower", price: 11.22, units: 10},
+      {symbol: "CAT", name: "Caterpillar Inc", price: 9.00, units: 100},
+      {symbol: "APPL", name: "Amazon", price: 234.00, units: 59},
+      {symbol: "AWK", name: "American Water", price: 100.00, units: 10}
+      ];
 /* add code here */
 
    //Since titles is simply an array of strings, it can be easily sorted.
@@ -39,14 +45,98 @@ document.addEventListener('DOMContentLoaded', () => {
    const paintingsSortedByYear = paintings.sort((a,b) =>  {
       return a.year < b.year ? -1 : 1; 
    });
-
    console.log(paintingsSortedByYear);
 
+   // This is using the filter function for the painting object , there should only be 5 objects in the array  
+   // To create a new array that is a subset of an existing array, you can use the filter()
+   const nineteenthCentury = paintings.filter(
+      painting => painting.year >= 1800 && painting.year < 1900);
    
+   console.log(nineteenthCentury);
+   
+   //To find the first element in an array that matches a criteria, you can use the find()
+   const manet = paintings.find( painting => painting.artist == "Manet");
+   console.log(manet);
+
+   // This will print out all the titles with the artist 
+   paintings.forEach(
+      p => { console.log(p.title + " by " + p.artist) });
 
 
+   // Unlike a regular for loop, you cannot break from a forEach().
+   /*
+   let indexBronz = 0;
+   paintings.forEach( p => {
+   if (p.artist == "Bronzino")
+      break; 
+   indexBronz++;
+   });
+   console.log("index of Bronzino="+indexBronz);
+    */ 
+
+   // This is better way to find the index for artist  Bronzino
+   let indexBronz = 0;
+   paintings.forEach( (p,index) => {
+      if (p.artist == "Bronzino") 
+         indexBronz = index;
+         
+      });
+   console.log("index of Bronzino = "+indexBronz);
 
 
+   // first define regular expression pattern ('i' = case insensitive),This uses regular expressions to search for all paintings whose title contains the word ‘ea’.
+   const re = new RegExp('ea','i');
+   const results = paintings.filter( p => p.title.match(re) );
+   console.log(results);
+
+   // using the spread operator, this will add the rest of the contents from title array into moreTitles array. This will also but the tiles array in order 
+   const moreTitles = ["Balcony", "Sunflowers", ...titles];
+   console.log(moreTitles);
+
+   // The same exact result just a different way of doing it 
+   const moreTitlesOldFashioned = ["Balcony", "Sunflowers"];
+   for (let t of titles) {
+      moreTitlesOldFashioned.push(t);
+   }
+   console.log(moreTitlesOldFashioned);
+
+   // concise way of doing this is via the map() function
+   const artists2 = paintings.map( painting => painting.artist);
+   console.log(artists2);
+
+   //create an array of DOM nodes that we will populate the existing <ul>
+   const artistDOMList = paintings.map( p => {
+      const li = document.createElement('li');
+      li.textContent = p.title;
+      li.value = p.id;
+      return li;
+      });
+
+   const list = document.querySelector("#list");
+   artistDOMList.forEach( li => { list.appendChild(li)});
+
+   // TEST YOUR KNOWLEDGE #1
+
+   for(let stock of stocks){
+      stock['total'] = stock['price'] * stock['units'];  
+   }
+   console.log('For of loop')
+   console.log(stocks)
+
+   stocks.forEach(stock => stock['total'] = stock['price'] * stock['units'])
+   
+   console.log('For each loop')
+   console.log(stocks)
+
+   const findCatSymbol = stocks.find( stock => stock['symbol'] == 'CAT')
+   console.log(findCatSymbol)
+
+   for(let s of stocks){
+      if (s['symbol'] == 'CAT')
+         console.log(s)
+   }
+
+   con
 
 
 });
