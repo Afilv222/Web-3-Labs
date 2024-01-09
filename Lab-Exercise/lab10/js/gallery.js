675c429b95f26842ced2459038f2543846a9819a
Afilv222 <55000122+Afilv222@users.noreply.github.com>
2024-01-08 22:18:12 -0700

    Starting the labs

--- Lab-Exercise/lab10/js/gallery.js
+++ Lab-Exercise/lab10/js/gallery.js
@@ -1,26 +1,5 @@
 // add class definition here (must be before you use it)
-class GalleryItem {
-   constructor(id, name){
-       this.id = id;
-       this.name = name;
-   }
 
-   render(){
-       let li = document.createElement("li");
-       li.textContent = this.name;
-       li.setAttribute("data-id", this.id);
-
-       return li; 
-   }
-
-   test(){
-      console.log(hi);
-   }
-
-}
-
-// add module code here
-const getSampleGalleries = () => galleries;
 
 // sample data using your class
 const galleries = [
@@ -33,5 +12,4 @@ const galleries = [
    new GalleryItem( 30,  "Rijksmuseum"  )
 ];
 
-
-export {GalleryItem, getSampleGalleries};
+// add module code here
--- Lab-Exercise/lab10/js/lab10-ex03.js
+++ Lab-Exercise/lab10/js/lab10-ex03.js
@@ -1,12 +1,18 @@
 // import required info from your module
-// import getSampleColors  from "./color.js";
-// import {getSampleColors}  from "./color.js";
-import * as colors from "./color.js";
+
 
 document.addEventListener("DOMContentLoaded", () => {
-   // use that function 
-   const container = document.querySelector("#list");
-   colors.getSampleColors().forEach( (c) => { 
-      c.createBox(container); 
-   });   
+   
+
+   
+   
+   function createBox(hex,luminance) {
+      const div = document.createElement('div');
+      div.classList.add('box');
+      div.style.backgroundColor = hex;
+      if (luminance < 80) div.style.color = 'white';
+      div.textContent = luminance.toFixed(2);
+      document.querySelector("#list").appendChild(div);
+   }
+   
 });
