9ecf7ae00fa6b01074151a47695915f017b8e361
Afilv222 <55000122+Afilv222@users.noreply.github.com>
2024-01-12 14:32:26 -0700

    Finished doing ex03

--- Lab-Exercise/lab10/js/gallery.js
+++ Lab-Exercise/lab10/js/gallery.js
@@ -1,4 +1,22 @@
 // add class definition here (must be before you use it)
+class GalleryItem{
+
+   constructor(id,name){
+      this.id = id;
+      this.name = name; 
+   }
+
+   render(){
+     const list = document.createElement('li'); 
+     const attributeName = list.setAttribute('data-id',this.id)
+     list.textContent = this.name
+     return list 
+
+   } 
+   
+}
+
+
 
 
 // sample data using your class
@@ -13,3 +31,10 @@ const galleries = [
 ];
 
 // add module code here
+
+//Make a arrow function 
+const getSampleGalleries = () => galleries;
+
+
+
+export{GalleryItem,getSampleGalleries};
--- Lab-Exercise/lab10/js/lab10-test02.js
+++ Lab-Exercise/lab10/js/lab10-test02.js
@@ -1,5 +1,6 @@
 // import the module here
-
+import {getSampleGalleries } from "./gallery.js";
+//import * as gallery from "./gallery.js";
 
 document.addEventListener("DOMContentLoaded", function() {
    // used the getSampleGalleries function from the gallery module
@@ -7,9 +8,12 @@ document.addEventListener("DOMContentLoaded", function() {
 
    // you'll be adding code to this <ul> list
    const list = document.querySelector("#galleryList");
-    
-   // now loop through the sample gallery data calling render and list.appendChild
 
+   // now loop through the sample gallery data calling render and list.appendChild
+   getSampleGalleries().forEach(gallery => {
+      console.log(gallery)
+      list.appendChild(gallery.render())
+   })
     
 
 });
