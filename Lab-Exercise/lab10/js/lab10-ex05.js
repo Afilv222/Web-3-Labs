36f3be83e0bd3af74e29fc44c265055f4a336fbb
Afilv222 <55000122+Afilv222@users.noreply.github.com>
2024-01-12 16:34:09 -0700

    Finished doing ex05

--- Lab-Exercise/lab10/js/lab10-ex05.js
+++ Lab-Exercise/lab10/js/lab10-ex05.js
@@ -5,6 +5,55 @@ const url = 'https://www.randyconnolly.com/funwebdev/3rd/api/travel/continents.p
 document.addEventListener("DOMContentLoaded", function() {
     
     // fetch the continents from the api in url
-
+    // To simplify the code for explanatory purposes, we are simplifying the error handling
     
+    fetch(url)
+        .then( (resp) => resp.json() )
+        .then( data => { 
+            displayContinents(data) 
+            clickContinents()
+        } )
+        .catch(error => console.error(error));
+
+    function displayContinents(continents) {
+        
+        const list = document.querySelector('.box ul');
+        console.log('During Fetch')
+        for (let c of continents) {
+            const item = document.createElement('li');
+            item.textContent = c.name;
+            list.appendChild(item);
+        }
+
+        } 
+    function clickContinents(){
+        
+        const items = document.querySelectorAll('.box ul li');
+        console.log(items);
+        
+        for (let li of items) {
+            li.addEventListener('click', (e) => {
+            console.log(e.target.textContent);
+            })
+        }
+    }
+
+    // this doesn't work ... why not?
+    /*
+        let fetchedData;
+        fetch(url)
+            .then( (resp) => resp.json() )
+            .then( data => {
+            // save fetched data in global variable
+            fetchedData = data;
+            console.log('got the data'); 
+
+            })
+            .catch(error => console.error(error));
+
+        // this looks like it should work but it doesn't
+        console.log('after fetch');
+        console.log(fetchedData); 
+        displayContinents(fetchedData);
+    */
 });
