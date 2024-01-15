7cc763a45ee107ad8b4811a74669408a08f83a5c
Afilv222 <55000122+Afilv222@users.noreply.github.com>
2024-01-14 20:47:53 -0700

    Finished doing ex10

--- Lab-Exercise/lab10/js/lab10-ex10.js
+++ Lab-Exercise/lab10/js/lab10-ex10.js
@@ -10,10 +10,12 @@ document.addEventListener("DOMContentLoaded", function() {
    filters.style.display = "none";
    select2.style.display = "none";    
 
+
+   // You can do this with anonymous function button.addEventListener('click', async () => {
    button.addEventListener('click', displayCountries);
+   
 
-
-
+   /*
    function displayCountries() {
       fetch(countryAPI)
          .then( resp => resp.json() )
@@ -29,6 +31,58 @@ document.addEventListener("DOMContentLoaded", function() {
             select1.addEventListener('input', displayCities);
          })
    }
+   */
+   
+   async function getCountryData() {
+      const response = await fetch(countryAPI);
+      console.log('after first await'); 
+      const data = await response.json();
+      console.log('after second await')
+      return data;
+   }
+   /*
+   function displayCountries() {
+      
+      console.log('before getCountryData'); 
+      const countries = getCountryData();
+      filters.style.display = "block";
+      console.log('after getCountryData'); 
+
+      
+      countries.forEach( c => {
+         const opt = document.createElement('option');
+         opt.setAttribute('value',c.iso );
+         opt.textContent = c.name;
+         select1.appendChild(opt);
+      });
+
+       // add a listener to the select
+      select1.addEventListener('input', displayCities);
+     }
+   */
+  
+   //// You can do this with anonymous function button.addEventListener('click', async () => {
+   async function displayCountries() {
+      try { 
+         const countries = await getCountryData();
+      
+         filters.style.display = "block";
+
+         countries.forEach( c => {
+            
+            const opt = document.createElement('option');
+            opt.setAttribute('value',c.iso );
+            opt.textContent = c.name;
+            
+            select1.appendChild(opt);
+         });
+
+         select1.addEventListener('input', displayCities);
+      }
+      catch (err) {
+         console.error(err);
+      } 
+   } 
 
    function displayCities() {
       const iso = select1.value;
